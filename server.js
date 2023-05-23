4e323374e55e4b05d487603ffd68a69ae57cca46
smartdev0612 <danieldev0612@gmail.com>
2023-05-22 19:38:55 -0700

    create routing

--- server.js
+++ server.js
@@ -2,4 +2,24 @@ const express = require('express')
 
 const app = express()
 
+app.use((req, res, next) => {
+  console.log('<h1>Helllooooo</h1>')
+})
+
+app.get('/', (req, res) => {
+  res.send('Getting root')
+})
+
+app.get('/profile', (req, res) => {
+  res.send('Getting profile')
+})
+
+app.post('/profile', (req, res) => {
+  const user = {
+    name: 'Sally',
+    hobby: 'soccer',
+  }
+  res.send(user)
+})
+
 app.listen(3001)
