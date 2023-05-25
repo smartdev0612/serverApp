7734dc5728eef744fae9bdab99c3fb00cd1eb2ef
smartdev0612 <danieldev0612@gmail.com>
2023-05-24 19:03:06 -0700

    create public directory

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+app.use(express.static(__dirname + '/public'))
 
 app.use((req, res, next) => {
   next()
